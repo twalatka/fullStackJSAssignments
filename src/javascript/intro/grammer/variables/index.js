21424a9e80cedbb354b7a8183cd7341a185c0599
twalatka <twalatka@gmail.com>
2017-01-31 19:46:54 -0700

    did assignment for Jan 31

---
+++ src/javascript/intro/grammer/variables/index.js
@@ -0,0 +1,16 @@
+// export var message = 'hello';
+
+// export let greeting = 'Yo!';
+
+// export const saying = 'What up';
+
+// message = 'world';
+// greeting = 'Hello';
+// // saying = 'Booyah'; cannot change a constant
+
+// OBSERVATIONS
+// 1. Var vaiables have their scocpe in the entire closing function, to all the sub-blocks within the closing function.  
+//     Let has its scope only within the sub-blocks within the closing funtion of the code. 
+// 2. You can update or change a Let variable, a Const cannot be changed, hence the reason it is called a Constant.
+// 3. Const should be used if you want your code to remain unchanged, Let will not protect your variable value, use Let when you 
+//    are ok with allowing the value of the variable to be changed.
